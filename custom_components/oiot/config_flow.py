8bc3cebd88c50db64c98d4e8a471dc79aa8543e3
Serge Arbuzov <Serge.Arbuzov@spacebridge.com>
2022-02-19 15:18:43 +0300

    Another cleanup
    Signed-off-by: Serge Arbuzov <Serge.Arbuzov@spacebridge.com>

--- custom_components/oiot/config_flow.py
+++ custom_components/oiot/config_flow.py
@@ -52,7 +52,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id='user', data_schema=STEP_USER_DATA_SCHEMA
             )
 
         errors = {}
